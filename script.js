60d5cdd443bd0677f99ce81e910b358cb314c673
janethxuiz <janethruiz152@gmail.com>
2022-09-02 12:23:47 -0700

    added some comments to code

--- script.js
+++ script.js
@@ -9,49 +9,16 @@
 // var lowercase = confirm("Use lowercase letters?")
 // var numbers = confirm("Use numbers?")
 // var symbols = confirm("Use special characters?")
-// }
-// if (uppercase != true && lowercase != true && numbers != true symbols != true){
-//     alert("Select at least one charater type")
-// }
-// }
-
-// const resultEl = document.getElementById('password');
-// document.getElementById('generate').addEventListener('click', () => {
-//     const hasUpper = uppercase.true;
-//     const hasLow = lowercase.true;
-//     const hasNumber = numbers.true;
-//     const hasSymbol = symbols.true;
-//     resultEl.innerText = generatePassword(hasUpper, hasLow, hasNumber, hasSymbol, length);
-// });
-
-// const passFunctions = {
-//     upper: getRandomUpper,
-//     lower: getRandomLower,
-//     number: getRandomNumber,
-//     symbol: getRandomSymbol
-// }
-
-// function generatePassword(upper, lower, number, symbol, length) {
-//     let generatePassword = '';
-//     const typesCount = upper + lower + number + symbol;
-//     const typesArr = [{upper}, {lower}, {number}, {symbol}].filter(item => Object.values(item)[0]);
-
-//     for(let i=0 i<length; i+typesCount) {
-//         typesArr.forEach(type => {
-//             const funName = Object.keys(type)[0];
-//         });
-//     }
-//     const
-// }
 
 var generateBtn = document.querySelector("#generate");
-
+// all password possibilities
 var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
 ];
 
+// This will pop up alert and ask all questions
 function questions() {
   var isValid = false;
   do {
@@ -78,6 +45,7 @@ function questions() {
   return responses;
 }
 
+// this will get all prompts and generate pw
 function generatePassword() {
   var passwordOptions = questions();
   var possibleCombo = [];
@@ -118,4 +86,5 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// event listener
 generateBtn.addEventListener("click", writePassword);
